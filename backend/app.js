b6298e1e74edf7062757879a065228be209a148a
NEEL <ee1924@cemk.ac.in>
2022-07-02 00:31:18 +0530

    minor changes made as per required

--- backend/app.js
+++ backend/app.js
@@ -44,27 +44,6 @@ app.get("/file/:filename", async (req, res) => {
     }
 })
 
-// app.get('/file/:filename', (req, res) => {
-//     gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
-//       // Check if file
-//       if (!file || file.length === 0) {
-//         return res.status(404).json({
-//           err: 'No file exists'
-//         });
-//       }
-  
-//       // Check if image
-//       if (file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
-//         // Read output to browser
-//         const readstream = gfs.createReadStream(file.filename);
-//         readstream.pipe(res);
-//       } else {
-//         res.status(404).json({
-//           err: 'Not an image'
-//         });
-//       }
-//     });
-//   });
 const port = 5000;
 app.listen(port, () => {
     console.log(`application is listening at port number ${port}`);
--- src/components/Images.js
+++ src/components/Images.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 
 const Images = () => {
-  const [docs, setDocs] = useState([]);
+  const [docs, setDocs] = useState(null);
   useEffect(() => {
     const url = "http://localhost:5000/"
     const config = {
@@ -11,8 +11,7 @@ const Images = () => {
       }
     }
     axios.get(url, config).then((response) => {
-      setDocs(response.data)
-      // console.log(response.data)
+      setDocs(response.data);
     });
   }, [])
   return (
@@ -20,15 +19,16 @@ const Images = () => {
       <div className='container'>
         <div className='row'>
           {
-            !docs ? " Nothing" : docs.map((item) => {
+            docs ? docs.map((item) => {
               return (
                 <div className='col-md-4' key={item._id}>
                   <div className="card" style={{ width: "30rem", marginTop: "10px" }}>
-                    <img src={`http://localhost:5000/file/${item.filename}`} className="card-img-top" alt="img" />
+                    <div style={{ display: 'flex', justifyContent: 'flex-end', position: 'absolute', right: '0' }} />
+                    <img src={`http://localhost:5000/file/${item.filename}`} alt="img" />
                   </div>
                 </div>
               )
-            })
+            }) : "Nothing"
           }
         </div>
       </div>
